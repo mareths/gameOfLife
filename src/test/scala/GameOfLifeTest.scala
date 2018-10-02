d086bf887db46e40a7a983ae4f9e37edfffab106
Mareths <sfret44@gmail.com>
2018-10-02 23:57:49 +0200

    Modif de la chaine de caractere à verifier pour verifier le build travis

--- src/test/scala/GameOfLifeTest.scala
+++ src/test/scala/GameOfLifeTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FlatSpec
 class GameOfLifeTest extends FlatSpec {
   "Quand on donne en entrée une chaine de caractère" should
   "retourne la chaine de carcatere" in {
-    val chaineAAfficher: String = "Salut"
+    val chaineAAfficher: String = "Hello"
     assert(GameOfLife.affiche(chaineAAfficher).equals(chaineAAfficher.toString))
   }
 
