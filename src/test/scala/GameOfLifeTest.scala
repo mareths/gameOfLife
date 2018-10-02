9bc2b7387637871eb44578a84f2357a8edf97655
Mareths <sfret44@gmail.com>
2018-10-03 00:01:21 +0200

    Test travis

--- src/test/scala/GameOfLifeTest.scala
+++ src/test/scala/GameOfLifeTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FlatSpec
 class GameOfLifeTest extends FlatSpec {
   "Quand on donne en entrée une chaine de caractère" should
   "retourne la chaine de carcatere" in {
-    val chaineAAfficher: String = "Hello"
+    val chaineAAfficher: String = "Salut"
     assert(GameOfLife.affiche(chaineAAfficher).equals(chaineAAfficher.toString))
   }
 
