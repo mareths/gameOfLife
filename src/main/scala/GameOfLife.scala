b2a29e7be3e9c17910a78bf6673c903697eea458
Mareths <sfret44@gmail.com>
2018-10-03 22:33:20 +0200

    Ajout parcours d'une chaine de caractere

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -1,4 +1,12 @@
 object GameOfLife {
+  def afficheCaractere(entree: String, pos: Option[Int] = Some(1)): Char = {
+    pos.get match {
+      case p if p > 0 & p <= entree.length => entree.charAt(p-1)
+      case p if p < entree.length => '.'
+      case _ => '.'
+    }
+  }
+
   def affiche(chaineAAfficher: String): String = {
     chaineAAfficher
   }
--- src/test/scala/GameOfLifeTest.scala
+++ src/test/scala/GameOfLifeTest.scala
@@ -3,9 +3,45 @@ import org.scalatest.FlatSpec
 
 class GameOfLifeTest extends FlatSpec {
   "Quand on donne en entrée une chaine de caractère" should
-  "retourne la chaine de carcatere" in {
+  "retourne la chaine de caractere" in {
     val chaineAAfficher: String = "Salut"
     assert(GameOfLife.affiche(chaineAAfficher).equals(chaineAAfficher.toString))
+  }
+
+  "Quand on donne en entrée une chaine de caractère" should
+  "retourne la premiere lettre de la chaine de caractere" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaractere(entree).equals('S'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère" should
+    "retourne la lettre de la chaine de caractere à la position demandé" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaractere(entree, Some(3)).equals('l'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position à 0" should
+    "retourne le caractère '.'" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaractere(entree, Some(0)).equals('.'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position à 1" should
+    "retourne le premier caractère" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaractere(entree, Some(1)).equals('S'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position égale à la longueur de la chaine de caractère" should
+    "retourne le dernier caractère" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaractere(entree, Some(5)).equals('t'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position superieure à la longueur de la chaine de caractère" should
+    "retourne le caractère '.'" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaractere(entree, Some(6)).equals('.'))
   }
 
 }
