c28920f813dd4e32a0c40ce0f4af7b80c42eac11
Mareths <sfret44@gmail.com>
2018-10-11 23:19:21 +0200

    Ajout du comptage des cellules voisines et création de la nouvelle génération avec 4 cellules

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -1,7 +1,86 @@
 object GameOfLife {
+  def convertirCompte(ligneComplet: String, ligneCompte: String, pos: Option[Int] = Some(0)): String = {
+    if (pos.get >= ligneCompte.length) ""
+    else {
+      var cell: String = {
+        if (ligneComplet.charAt(pos.get).equals(".")) {
+          ligneCompte.charAt(pos.get) match {
+            case '3' => "*"
+            case _ => "."
+          }
+        } else {
+          ligneCompte.charAt(pos.get) match {
+            case '2' => "*"
+            case '3' => "*"
+            case _ => "."
+          }
+        }
+      }
+      cell.concat(convertirCompte(ligneComplet, ligneCompte, Some(pos.get + 1)))
+    }
+  }
+
+  def compteLesCellulesVivantesVoisines(ligneComplet: String, pos: Option[Int] = Some(0)): String = {
+    if (pos.get >= ligneComplet.length) ""
+    else {
+      var count: Int =
+        pos.get match {
+          case p if p > 0 & p <= ligneComplet.length  =>
+            ligneComplet.charAt(p-1) match {
+              case '.' => 0
+              case '*' => 1
+              case _ => 0
+            }
+          case _ => 0
+        }
+        count += {
+          pos.get match {
+            case p if p >= 0 & p < ligneComplet.length -1 =>
+              ligneComplet.charAt(p+1) match {
+                case '.' => 0
+                case '*' => 1
+                case _ => 0
+              }
+            case _ => 0
+          }
+        }
+      count.toString.concat(compteLesCellulesVivantesVoisines(ligneComplet, Some(pos.get + 1)))
+    }
+  }
+
+
+  def compteLesCellulesVivantes(entree: String): String = {
+    if (entree.isEmpty) ""
+    else {
+      val count: String =
+        entree.head match {
+          case '.' => "0"
+          case '*' => "1"
+          case _ => ""
+        }
+      count.concat(compteLesCellulesVivantes(entree.drop(1)))
+    }
+  }
+
+  def afficheCaractereSuivant(entree: String, pos: Some[Int]): Char = {
+    pos.get match {
+      case p if p >= 0 & p < entree.length => afficheCaractere(entree, Some(pos.get + 1))
+      case p if p < entree.length + 1 => '.'
+      case _ => '.'
+    }
+  }
+
+  def afficheCaracterePrecedent(entree: String, pos: Some[Int]): Char = {
+    pos.get match {
+      case p if p > 1 & p <= entree.length + 1 => afficheCaractere(entree, Some(pos.get - 1))
+      case p if p < entree.length + 1 => '.'
+      case _ => '.'
+    }
+  }
+
   def afficheCaractere(entree: String, pos: Option[Int] = Some(1)): Char = {
     pos.get match {
-      case p if p > 0 & p <= entree.length => entree.charAt(p-1)
+      case p if p > 0 & p <= entree.length => entree.charAt(p - 1)
       case p if p < entree.length => '.'
       case _ => '.'
     }
--- src/test/scala/GameOfLifeTest.scala
+++ src/test/scala/GameOfLifeTest.scala
@@ -2,6 +2,7 @@ import org.scalatest.FlatSpec
 
 
 class GameOfLifeTest extends FlatSpec {
+
   "Quand on donne en entrée une chaine de caractère" should
   "retourne la chaine de caractere" in {
     val chaineAAfficher: String = "Salut"
@@ -42,6 +43,108 @@ class GameOfLifeTest extends FlatSpec {
     "retourne le caractère '.'" in {
     val entree: String = "Salut"
     assert(GameOfLife.afficheCaractere(entree, Some(6)).equals('.'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position à 3" should
+    "retourne le caractère precedent" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaracterePrecedent(entree, Some(3)).equals('a'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position à 3" should
+    "retourne le caractère suivant" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaractereSuivant(entree, Some(3)).equals('u'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position à 5" should
+    "ne peux pas retourner le caractère suivant" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaractereSuivant(entree, Some(5)).equals('.'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position à 1" should
+    "ne peux pas retourner le caractère precedent" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaracterePrecedent(entree, Some(1)).equals('.'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position à 4" should
+    "retourne le caractère suivant" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaractereSuivant(entree, Some(4)).equals('t'))
+  }
+
+  "Quand on donne en entrée une chaine de caractère et une position à 2" should
+    "retourne le caractère precedent" in {
+    val entree: String = "Salut"
+    assert(GameOfLife.afficheCaracterePrecedent(entree, Some(2)).equals('S'))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \"....\"" should
+    "retourne la chaine de carcatere \"0000\"" in {
+    val entree: String = "...."
+    assert(GameOfLife.compteLesCellulesVivantes(entree).equals("0000"))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \".*..\"" should
+    "retourne la chaine de carcatere \"0100\"" in {
+    val entree: String = ".*.."
+    assert(GameOfLife.compteLesCellulesVivantes(entree).equals("0100"))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \"....\"" should
+    "retourne la chaine de carcatere \"0000\" qui indique qu'il n'y a aucune cellules vivantes voisines sur la ligne" in {
+    val entree: String = "...."
+    assert(GameOfLife.compteLesCellulesVivantesVoisines(entree).equals("0000"))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \".*..\"" should
+    "retourne la chaine de carcatere \"0100\" qui indique qu'à 2 endroits il y a une cellules voisine vivante" in {
+    val entree: String = ".*.."
+    assert(GameOfLife.compteLesCellulesVivantesVoisines(entree).equals("1010"))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \"0000\" et une chaine de caractère \"....\"" should
+    "traduit et retourne la chaine de carcatere \"....\" qui indique qu'aucune cellule ne va revivre" in {
+    val entree: String = "...."
+    val ligneCompte: String = "0000"
+    assert(GameOfLife.convertirCompte(entree, ligneCompte).equals("...."))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \"0000\" et une chaine de caractère \".*..\"" should
+    "traduit et retourne la chaine de carcatere \"....\" qui indique qu'aucune cellule ne va revivre" in {
+    val entree: String = ".*.."
+    val ligneCompte: String = "0000"
+    assert(GameOfLife.convertirCompte(entree, ligneCompte).equals("...."))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \"0100\" et une chaine de caractère \".*..\"" should
+    "traduit et retourne la chaine de carcatere \"....\" qui indique qu'aucune cellule ne va revivre" in {
+    val entree: String = ".*.."
+    val ligneCompte: String = "0100"
+    assert(GameOfLife.convertirCompte(entree, ligneCompte).equals("...."))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \"0200\" et une chaine de caractère \".*..\"" should
+    "traduit et retourne la chaine de carcatere \".*..\" qui indique qu'aucune cellule ne va revivre" in {
+    val entree: String = ".*.."
+    val ligneCompte: String = "0200"
+    assert(GameOfLife.convertirCompte(entree, ligneCompte).equals(".*.."))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \"0300\" et une chaine de caractère \".*..\"" should
+    "traduit et retourne la chaine de carcatere \".*..\" qui indique qu'aucune cellule ne va revivre" in {
+    val entree: String = ".*.."
+    val ligneCompte: String = "0300"
+    assert(GameOfLife.convertirCompte(entree, ligneCompte).equals(".*.."))
+  }
+
+  "Quand on donne en entrée la chaine de caractère \"0400\" et une chaine de caractère \".*..\"" should
+    "traduit et retourne la chaine de carcatere \"....\" qui indique qu'aucune cellule ne va revivre" in {
+    val entree: String = ".*.."
+    val ligneCompte: String = "0400"
+    assert(GameOfLife.convertirCompte(entree, ligneCompte).equals("...."))
   }
 
 }
